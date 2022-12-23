00eeec0ca58bb044152a4dbe892903cbbc03564a
Igor Kostyukov <ikostyukov@bkmedical.com>
2022-12-23 22:35:45 +0300

    correction of exception handling in the error handling middleware

--- src/OnlineSales/ErrorHandling/ErrorMessageMiddleware.cs
+++ src/OnlineSales/ErrorHandling/ErrorMessageMiddleware.cs
@@ -56,7 +56,8 @@ namespace OnlineSales.Controllers
             }
             catch (Exception ex)
             {
-                var em = errorMessageGenerator.CreateErrorMessage<InnerErrorCodes.Status400>(StatusCodes.Status500InternalServerError, InnerErrorCodes.Status500.ExceptionCaught, ex.GetType().Name);
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                var em = errorMessageGenerator.CreateErrorMessage<InnerErrorCodes.Status500>(StatusCodes.Status500InternalServerError, InnerErrorCodes.Status500.ExceptionCaught, ex.GetType().Name);
                 em.ErrorDescription = ex.Message;
                 await WriteToStream(JsonSerializer.Serialize(em, IErrorMessageGenerator.ErrorHandlingSerializerOptions), originBody);
                 context.Response.Body = originBody;
